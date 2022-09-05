2061e96d610c9b84be82408f07300e6c1b2fb4e9
lukas175 <lukasersej175@gmail.com>
2022-09-05 13:30:34 +0200

    leapyear

---
+++ BDSA-Exercises/LeapYear.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ConsoleApp
+{
+    public class LeapYear
+    {
+
+        public bool IsLeapYear(int year)
+        {
+            return year % 4 == 0 && (year % 100 == 0 ? year % 400 == 0 : true);
+        }
+    }
+}
---
+++ TestProject/LeapYearTests.cs
@@ -0,0 +1,73 @@
+﻿
+using ConsoleApp;
+
+namespace Tests
+{
+    public class LeapYearTests
+    {
+        [Fact]
+        public void IsLeapYear_2000_true()
+        {
+            //Assign
+            var leapCalculator = new LeapYear();
+
+            //Act
+            bool isLeapyear = leapCalculator.IsLeapYear(2000);
+
+            //Assert
+            Assert.True(isLeapyear);
+        }
+
+        [Fact]
+        public void IsLeapYear_2004_true()
+        {
+            //Assign
+            var leapCalculator = new LeapYear();
+
+            //Act
+            bool isLeapyear = leapCalculator.IsLeapYear(2004);
+
+            //Assert
+            Assert.True(isLeapyear);
+        }
+
+        [Fact]
+        public void IsLeapYear_2001_false()
+        {
+            //Assign
+            var leapCalculator = new LeapYear();
+
+            //Act
+            bool isLeapyear = leapCalculator.IsLeapYear(2001);
+
+            //Assert
+            Assert.False(isLeapyear);
+        }
+
+        [Fact]
+        public void IsLeapYear_1700_false()
+        {
+            //Assign
+            var leapCalculator = new LeapYear();
+
+            //Act
+            bool isLeapyear = leapCalculator.IsLeapYear(1700);
+
+            //Assert
+            Assert.False(isLeapyear);
+        }
+
+        [Fact]
+        public void IsLeapYear_1704_true()
+        {
+            //Assign
+            var leapCalculator = new LeapYear();
+
+            //Act
+            bool isLeapyear = leapCalculator.IsLeapYear(1704);
+
+            //Assert
+            Assert.True(isLeapyear);
+        }
+    }
+}
